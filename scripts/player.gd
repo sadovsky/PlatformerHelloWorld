dff07bdb91691855005a446ae247c48cd706e005
Alex Sadovsky <alexander.sadovsky@gmail.com>
2025-04-25 19:55:24 -0600

    more progression and a couple monster art designs from silas

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor()  :
 		velocity += get_gravity() * delta
 
 	# Handle jump.
